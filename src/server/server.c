33209cb6b97b6c3e04d69f616a7c1b69dbc9de4a
Alexandre BM <s@rednaks.tn>
2014-05-09 11:57:14 +0100

    Implementation de la fonction |recevoirMsg|

--- src/server/server.c
+++ src/server/server.c
@@ -29,3 +29,16 @@ int init_server(int portNum) {
 
   return sockfd;
 }
+
+int recevoirMsg(int sockfd, char* message, int size) {
+  bzero(message, size);
+
+  int res = read(sockfd, message, size);
+
+  if(res < 0) {
+    return RECV_ERR;
+  }
+
+  return res;
+}
+
--- src/server/server.h
+++ src/server/server.h
@@ -6,6 +6,7 @@
 
 #define PORT_BIND_ERR               -2
 #define LISTEN_ERR                  -3
+#define RECV_ERR                    -4
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -27,5 +28,13 @@ ClientInfo ClientRang[MAX_CONNECTION];
     LISTEN_ERR : s'il n'est pas possible d'écouter
 */
 int init_server(int portNum);
+
+/*
+  recevoirMsg(sockfd, message, taille):
+    Le message reçu sera mis dans la variable |message|.
+    n : le nombre d'octets reçus
+    RECV_ERR : s'il y a eu une erreur lors de la reception.
+*/
+int recevoirMsg(int sockfd, char* message, int size);
 
 #endif
