ef41f3c79fefe27e3e2136c0b939b728ee92f5ef
Alexandre BM <s@rednaks.tn>
2014-05-09 11:56:15 +0100

    Ajout d'un test pour la reception

--- src/server/main.c
+++ src/server/main.c
@@ -1,10 +1,30 @@
+#include <stdio.h>
+
 #include "server.h"
+
 
 
 int main(int argc, char **argv) {
 
   int res = init_server(9999);
   printf("Res = %d\n", res);
+
+  int taille = 256;
+  char message[taille];
+
+  int client_sockfd, client_len;
+  int client_addr;
+  client_len = sizeof(client_addr);
+
+  client_sockfd = accept(res, (struct sockaddr*) &client_addr, &client_len);
+  int n = recevoirMsg(client_sockfd, message, taille);
+
+  if(n < 0){
+    perror("RES");
+  }
+
+  printf("J'ai reçu : %d octets\n", n);
+  printf("Le message est : %s\n", message);
 
   return 0;
 }
